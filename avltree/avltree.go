bbc768436d92096446ccc63b5d64ac28c664a75d
grbll@posteo.com <grbll@posteo.com>
2024-10-30 22:21:37 +0100

    again noticed basic design problems...need to refactor

--- avltree/avltree.go
+++ avltree/avltree.go
@@ -2,15 +2,47 @@ package avltree
 
 import ibs "github.com/grbll/go-bstree/internal/internalbstree"
 
-type Comparable[T any] interface {
-	Less(T) bool
-}
-
-type avlNode[T Comparable[T]] struct {
+type avlNode[T ibs.Comparable[T]] struct {
 	ibs.TreeNode[T]
 	balance int
 }
 
-func createAVLNode[T Comparable[T]]() *avlNode[T] {
-	return &avlNode[T]{*ibs.NewTreeNode[T](), 0}
+func createAVLNode[T ibs.Comparable[T]]() *avlNode[T] {
+	return &avlNode[T]{TreeNode: *ibs.NewTreeNode[T](), balance: 0}
+}
+
+func (tree *avlNode[T]) Insert(value T) {
+	if tree.Value != nil {
+
+		var head **avlNode[T] = &tree
+
+		for {
+			if value.Less(*(*head).Value) { //a smaller value should go to the right side
+				if (*head).Left != nil {
+					head = &(*head).Left //if left child is not nil, we keep looking...
+				} else {
+					(*head).Left = &TreeNode[T]{Value: &value, Parent: *head, Left: nil, Right: nil} //here we can place our new node using the correct parent
+					return
+				}
+			} else {
+				if (*head).Right != nil { //...and a larger one to the right
+					head = &(*head).Right //if right child is not nil, we keep looking...
+				} else {
+					(*head).Right = &TreeNode[T]{Value: &value, Parent: *head, Left: nil, Right: nil} //here we can place our new node using the correct parent
+					return
+				}
+			}
+		}
+
+	} else {
+		tree.Value = &value // this should only happen for the root of a newly initiated tree
+	}
+	return
+
+}
+func NewAVLTree[T ibs.Comparable[T]](values []T) ibs.BSTree[T] {
+	// Use NewBSTree to initialize the tree with the creation function
+	tree := ibs.NewBSTree(values, func() ibs.BSTree[T] { return createAVLNode[T]() })
+	// Cast the returned TreeNode to *avlNode
+	return tree
 }
--- internal/internalbstree/bstree.go
+++ internal/internalbstree/bstree.go
@@ -12,10 +12,10 @@ type Comparable[T any] interface {
 
 // base struct for holding values
 type TreeNode[T Comparable[T]] struct {
-	value  *T
-	parent *TreeNode[T]
-	left   *TreeNode[T]
-	right  *TreeNode[T]
+	Value  *T
+	Parent *TreeNode[T]
+	Left   *TreeNode[T]
+	Right  *TreeNode[T]
 }
 
 // tree to string method
@@ -23,11 +23,11 @@ func (tree *TreeNode[T]) String() string {
 
 	if tree != nil {
 		sb := strings.Builder{}
-		sb.WriteString(fmt.Sprintf("%v", *tree.value))
+		sb.WriteString(fmt.Sprintf("%v", *tree.Value))
 		sb.WriteByte('[')
-		sb.WriteString(tree.left.String())
+		sb.WriteString(tree.Left.String())
 		sb.WriteByte(',')
-		sb.WriteString(tree.right.String())
+		sb.WriteString(tree.Right.String())
 		sb.WriteByte(']')
 
 		return sb.String()
@@ -43,30 +43,30 @@ func NewTreeNode[T Comparable[T]]() *TreeNode[T] {
 
 // insert method based on comparison
 func (tree *TreeNode[T]) Insert(value T) {
-	if tree.value != nil {
+	if tree.Value != nil {
 
 		var head **TreeNode[T] = &tree
 
 		for {
-			if value.Less(*(*head).value) { //a smaller value should go to the right side
-				if (*head).left != nil {
-					head = &(*head).left //if left child is not nil, we keep looking...
+			if value.Less(*(*head).Value) { //a smaller value should go to the right side
+				if (*head).Left != nil {
+					head = &(*head).Left //if left child is not nil, we keep looking...
 				} else {
-					(*head).left = &TreeNode[T]{value: &value, parent: *head, left: nil, right: nil} //here we can place our new node using the correct parent
+					(*head).Left = &TreeNode[T]{Value: &value, Parent: *head, Left: nil, Right: nil} //here we can place our new node using the correct parent
 					return
 				}
 			} else {
-				if (*head).right != nil { //...and a larger one to the right
-					head = &(*head).right //if right child is not nil, we keep looking...
+				if (*head).Right != nil { //...and a larger one to the right
+					head = &(*head).Right //if right child is not nil, we keep looking...
 				} else {
-					(*head).right = &TreeNode[T]{value: &value, parent: *head, left: nil, right: nil} //here we can place our new node using the correct parent
+					(*head).Right = &TreeNode[T]{Value: &value, Parent: *head, Left: nil, Right: nil} //here we can place our new node using the correct parent
 					return
 				}
 			}
 		}
 
 	} else {
-		tree.value = &value // this should only happen for the root of a newly initiated tree
+		tree.Value = &value // this should only happen for the root of a newly initiated tree
 	}
 	return
 
@@ -76,11 +76,12 @@ type BSTree[T Comparable[T]] interface {
 	Insert(T)
 }
 
-func NewBSTree[T Comparable[T]](values []T, create func() *TreeNode[T]) *TreeNode[T] {
+func NewBSTree[T Comparable[T]](values []T, create func() BSTree[T]) BSTree[T] {
 	tree := create()
 
 	for _, value := range values {
 		tree.Insert(value)
 	}
+
 	return tree
 }
